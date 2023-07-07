50fe2e1e6d6a7457cc0ede2b3faa2e1e2f0a7831
Snizhana-Mykhaylova <snizhana.mykhaylova@gmail.com>
2023-07-07 10:52:14 +0200

    increment und decrement

--- cart.php
+++ cart.php
@@ -46,6 +46,65 @@ if (isset($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'
     unset($_SESSION['cart'][$_GET['remove']]);
 }
 
+if (isset($_GET['increment']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['increment']])) {
+
+    var_dump($_GET);
+    var_dump($_SESSION['cart']);
+    $cart = $_SESSION['cart'];
+    foreach ($cart as $k => $quantity) {
+
+        if ($k == $_GET['increment']) {
+            $zahlQuantity = floatval($quantity);
+            $zahlQuantity++;
+
+            if (isset($_SESSION['cart'][$_GET['increment']]) && $quantity > 0) {
+                // increment new quantity
+                $_SESSION['cart'][$_GET['increment']] = $zahlQuantity;
+                var_dump($_SESSION['cart']);
+            }
+        }
+    }
+}
+$zahlQuantity = NULL;
+
+if (isset($_GET['decrement']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['decrement']])) {
+
+    var_dump($_GET);
+    var_dump($_SESSION['cart']);
+    $cart = $_SESSION['cart'];
+    foreach ($cart as $k => $quantity) {
+
+        if ($k == $_GET['decrement']) {
+            $zahlQuantity = floatval($quantity);
+            $zahlQuantity--;
+
+            if (isset($_SESSION['cart'][$_GET['decrement']]) && $quantity > 0) {
+                // increment new quantity
+                $_SESSION['cart'][$_GET['decrement']] = $zahlQuantity;
+                var_dump($_SESSION['cart']);
+            }
+        }
+    }
+
+
+
+    //  Loop through the post data so we can update the quantities for every product in cart
+    // foreach ($_POST as $k => $v) {
+    //     if (strpos($k, 'quantity') !== false) {
+    //         $id = str_replace('quantity-', '', $k);
+    //         $quantity++;
+    //         // Always do checks and validation
+    //         if (isset($_SESSION['cart'][$id]) && $quantity > 0) {
+    //             // Update new quantity
+    //             $_SESSION['cart'][$id] = $quantity;
+    //         }
+    //     }
+    // }
+    // // Prevent form resubmission...
+    // header('location: cart.php');
+    // exit;
+}
+
 // // Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
 // if (isset($_POST['update']) && isset($_SESSION['cart'])) {
 //     // Loop through the post data so we can update the quantities for every product in cart
@@ -121,18 +180,10 @@ if ($products_in_cart) {
         echo "<img width='50' height='50' class='rewiewImg' src='./bilder/waren/img_" . $product[0]['ArtikelID'] . ".png' alt=''>";
         echo "<div class='itemText'> <p class='itemName'>" . $product[0]['Artikelname'] . "</p> </div>";
         echo "<p class='preis'>$" . number_format($preis, 2, ',', '.') . "</p>";
-        echo "<input
-                      class='itemAnzahl'
-                      id='quantity'
-                      type='number'
-                      name='quantity'
-                      min='1'
-                      max='100'
-                      step='1'
-                      value=$quant
-                    />";
-
+        echo "<p class='preis'>" . $quant . "</p>";
         echo "<a class=button href='cart.php?remove=" . $product[0]['ArtikelID'] . "' class='remove'>Remove</a>";
+        echo "<a class=button href='cart.php?increment=" . $product[0]['ArtikelID'] . "' class='increment'>+</a>";
+        echo "<a class=button href='cart.php?decrement=" . $product[0]['ArtikelID'] . "' class='decrement'>-</a>";
         echo "</li>";
 
         // Calculate the zwischensumme 
